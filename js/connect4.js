28c865fa937fdd41052df64230ebfd0e27453930
mecharius <hart.wl@gmail.com>
2012-06-04 12:54:09 +0100

    Draw game board

--- js/connect4.js
+++ js/connect4.js
@@ -24,19 +24,24 @@ var YELLOW = 1;
 var RED = 2;
 
 /* define some sprite states */
-var EMPTY_CELL = 0; // a disc in the game board
-var FILL_CELL = 1; // a disc outside the game board
-var WINNING_CELL = 2; // a disc that is part of the winning combination
+var EMPTY_CELL = 0; 		// a disc in the game board
+var PLACE_CELL = 1; 		// where a disc may be placed
+var FILL_CELL = 2; 			// a disc outside the game board
+var WINNING_CELL = 3; 		// a disc that is part of the winning combination
 
 /* the number of sprites in any row */
-var ROW_MULTIPLIER = 3; // a multiplier for accessing different sprite rows
+var ROW_MULTIPLIER = 3; 	// a multiplier for accessing different sprite rows
 
 /* A blank cell where no sprite is shown */
 var BLANK_CELL = -1;
 
+/* the size of a cell (they are square) */
+var CELL_SIZE = 64;
+
 /* load the sprite sheet */
 var spriteImage = new Image();
 var spriteSheet;
+var animFrames;
 
 /* an 8x8 array to use to hold the game map */
 var map = []; 
@@ -90,25 +95,72 @@ function imgLoadError() {
  * 'Builds' the game by loading resources and setting up a timer
  */
 function buildGame() {
+	
+	// set up the 'animation' frames
+	animFrames = 
+	
 	// build a spritesheet up from the image we have loaded
     spriteSheet = new SpriteSheet({
 	    images: [spriteImage], 
-	    frames: {width: 64, height: 64, count: 9, regX: 0, regY: 0}, 
+	    frames: {width: 64, height: 64, count: 12, regX: 1, regY: 1},
+	    animations: {
+			blank: [9],
+			empty_cell: [0],
+			yellow_cell: [1],
+			red_cell: [2],
+			place_yellow: [4],
+			place_red: [5],
+			fill_yellow: [7],
+			fill_red: [8],
+			win_yellow: [10],
+			win_red: [11],
+		}
     });
     
     // build a stage object to hold our game board
     stage = new Stage(game);
+    stage.mouseEventsEnabled = true; // allow mouse events
     
     // set up a game ticker
     Ticker.addListener(window);
     Ticker.useRAF = true;
     Ticker.setFPS(60);
-    
+ 
     // initialise the game board
-    var emptyId = getSpriteIdx(EMPTY_CELL, NONE);
-    for (var i = 0; i < 9; i++) {
-    	map.push([emptyId, emptyId, emptyId, emptyId, emptyId, emptyId, emptyId, emptyId, BLANK_CELL, BLANK_CELL]);
+    for (var i = 2; i < 11; i++) {
+    	var line = [];
+    	for (var j = 0; j < 11; j++) {
+    		/// create a new bitmap animation
+    		var bma = new BitmapAnimation(spriteSheet);
+    		
+    		// set up the correct bitmap
+    		if (j == 0)
+    		{
+    			bma.gotoAndPlay("blank");
+    		} else if (j == 1){
+    			bma.gotoAndPlay("place_yellow");
+    		} else {
+    			bma.gotoAndPlay("empty_cell");
+    		}
+    		
+    		// configure the bitmap animation position
+    		bma.name="cell"+i+"-"+j;
+    		bma.x = i * CELL_SIZE;
+    		bma.y = j * CELL_SIZE;
+    		stage.addChild(bma);
+	    	line.push(bma);
+    	}
+    	map.push(line);
     }
+}
+
+
+/*
+ * The tick function updates the stage based on the game state
+ */
+function tick() {
+	// redraw the stage
+	stage.update();
 }
 
 
